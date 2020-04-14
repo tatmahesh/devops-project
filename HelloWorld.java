39c1229dfe49b6e6ecd4a264b70e179ca57251cc
tatmahesh <63229563+tatmahesh@users.noreply.github.com>
2020-04-14 12:30:58 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String args[]){
-		System.out.println("This is hello world java program with WebHook");
+		System.out.println("This is through webhook method");
 	}
 };
