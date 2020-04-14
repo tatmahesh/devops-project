528f42bde46b6b0d6548caa3f5711696b65da316
tatmahesh <63229563+tatmahesh@users.noreply.github.com>
2020-04-14 12:05:08 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String args[]){
-		System.out.println("This is hello world java program. Added for poll");
+		System.out.println("This is hello world java program with WebHook");
 	}
 };
