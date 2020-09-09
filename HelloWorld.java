95f21db02677a72778fcce70fd00b5051430774f
tatmahesh <63229563+tatmahesh@users.noreply.github.com>
2020-09-09 07:14:58 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String args[]){
-		System.out.println("This is through git poll SCM);
+		System.out.println("This is through git poll SCM)";
 	}
 };
