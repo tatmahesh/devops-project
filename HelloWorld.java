90fe720058b40a5c5c88510378a5f7ccaee4b0aa
tatmahesh <63229563+tatmahesh@users.noreply.github.com>
2020-09-09 07:48:51 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("This is through git poll SCM");
-		System.out.println("Added another line to git poll SCM");
 		System.out.println("Added another line for WebHook);
 	}
 };
